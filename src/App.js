882ebfea5781f287110c76a444077b247e7a2e85
David Gyulinyan <davidgyulinyan@gmail.com>
2024-01-20 15:41:41 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import SongList from "./components/SongList/SongList"
+
+function App() {
+  return (
+    <div className="App">
+      <main className="App-header">
+        <SongList/>
+      </main>
+    </div>
+  );
+}
+
+export default App;
